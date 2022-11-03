5604ed71453b07a67ed341f79ce943205f00b50d
Youn <pleiadex.dev@gmail.com>
2022-11-04 00:42:07 +0900

    build_qap eslint resolved

---
+++ src/build_qap.js
@@ -0,0 +1,268 @@
+import * as curves from './curves.js';
+import * as polyUtils from './utils/poly_utils';
+import chai from 'chai';
+const assert = chai.assert;
+import {readR1csHeader} from 'r1csfile';
+import {
+  readBinFile,
+  readSection,
+  createBinFile,
+  writeBigInt,
+  startWriteSection,
+  endWriteSection
+} from '@iden3/binfileutils';
+import {Scalar} from 'ffjavascript';
+import {mkdir} from 'fs';
+import path from 'path';
+import * as timer from './utils/timer.js';
+
+
+export default async function buildQAP(curveName, sD, minSMax) {
+  const startTime = timer.start();
+  let partTime;
+
+  // TODO: read debug mode from enviroment variable
+  const TESTFLAG = false;
+  const r1cs = [];
+  const sR1cs = [];
+
+  mkdir(
+      path.join(
+          `resource/subcircuits`, `QAP_${sD}_${minSMax}`,
+      ), (err) => {},
+  );
+  const dirPath = `resource/subcircuits/QAP_${sD}_${minSMax}`;
+
+  partTime = timer.start();
+  for (let i=0; i<sD; i++) {
+    console.log(`Loading R1CSs...${i+1}/${sD}`);
+    const r1csIdx = String(i);
+    const {
+      fd: fdR1cs,
+      sections: sectionsR1cs,
+    } = await readBinFile(
+        'resource/subcircuits/r1cs/subcircuit'+r1csIdx+'.r1cs',
+        'r1cs',
+        2,
+        1<<22,
+        1<<24,
+    );
+    r1cs.push(
+        await readR1csHeader(fdR1cs, sectionsR1cs, false),
+    );
+    sR1cs.push(
+        await readSection(fdR1cs, sectionsR1cs, 2),
+    );
+    await fdR1cs.close();
+  }
+  console.log(`Loading R1CSs...Done`);
+  const r1csTime = timer.end(partTime);
+
+  const fdRS = await createBinFile(
+      `resource/subcircuits/param_${sD}_${minSMax}.dat`,
+      'zkey',
+      1,
+      2,
+      1<<22,
+      1<<24,
+  );
+
+  const curve = await curves.getCurveFromName(curveName);
+  const Fr = curve.Fr;
+
+  if (r1cs[0].prime != curve.r) {
+    console.log('r1cs_prime: ', r1cs[0].prime);
+    console.log('curve_r: ', curve.r);
+    throw new Error(
+        'r1cs curve does not match powers of tau ceremony curve',
+    );
+    // return -1
+  }
+
+  // Write Header
+  // /////////
+  await startWriteSection(fdRS, 1);
+  await fdRS.writeULE32(1); // Groth
+  await endWriteSection(fdRS);
+  // End of the Header
+
+  // Write parameters section
+  // /////////
+  await startWriteSection(fdRS, 2);
+  const primeQ = curve.q;
+  const n8q = (Math.floor( (Scalar.bitLength(primeQ) - 1) / 64) +1)*8;
+
+  // Group parameters
+  const primeR = curve.r;
+  const n8r = (Math.floor( (Scalar.bitLength(primeR) - 1) / 64) +1)*8;
+
+  await fdRS.writeULE32(n8q); // byte length of primeQ
+  await writeBigInt(fdRS, primeQ, n8q);
+  await fdRS.writeULE32(n8r); // byte length of primeR
+  await writeBigInt(fdRS, primeR, n8r);
+
+  // Instruction set constants
+  await fdRS.writeULE32(sD);
+  const m = []; // the numbers of wires
+  const mPublic = []; // the numbers of public wires
+  // (not including constant wire at zero index)
+  const mPrivate = [];
+  const nConstraints = [];
+  for (let i=0; i<sD; i++) {
+    m.push(r1cs[i].nVars);
+    nConstraints.push(r1cs[i].nConstraints);
+    mPublic.push(r1cs[i].nOutputs + r1cs[i].nPubInputs + r1cs[i].nPrvInputs);
+    mPrivate.push(m[i] - mPublic[i]);
+    await fdRS.writeULE32(m[i]);
+    await fdRS.writeULE32(mPublic[i]);
+    await fdRS.writeULE32(nConstraints[i]);
+  }
+
+  // QAP constants
+  //   const sum_mPublic = mPublic.reduce((accu, curr) => accu + curr);
+  //   const sum_mPrivate = mPrivate.reduce((accu, curr) => accu + curr);
+  //   const NEqs = Math.max(
+  //     mPublic.reduce((accu, curr) => accu + curr),
+  //     mPrivate.reduce((accu, curr) => accu + curr));
+  let n = Math.max(...nConstraints);
+
+  const expon = Math.ceil(Math.log2(n));
+  n = 2**expon;
+
+  const omegaX = await Fr.exp(Fr.w[Fr.s], Scalar.exp(2, Fr.s-expon));
+
+  const expos = Math.ceil(Math.log2(minSMax));
+  const sMax = 2**expos;
+  const omegaY = await Fr.exp(Fr.w[Fr.s], Scalar.exp(2, Fr.s-expos));
+
+  // TODO: chai should not be used for production code
+  if (TESTFLAG) {
+    console.log(`Running Test 1`);
+    assert(Fr.eq(await Fr.exp(Fr.e(n), primeR), Fr.e(n)));
+    assert(Fr.eq(await Fr.exp(Fr.e(omegaX), n), Fr.one));
+    assert(Fr.eq(await Fr.exp(Fr.e(omegaY), sMax), Fr.one));
+    console.log(`Test 1 finished`);
+  }
+  // End of test code 1 //
+
+  // the maximum number of gates in each subcircuit:
+  // n>=NEqs/3 and n|(r-1)
+  await fdRS.writeULE32(n);
+
+  // the maximum number of subcircuits in a p-code:
+  // sMax>minSMax and sMax|(r-1)
+  await fdRS.writeULE32(sMax);
+
+  // Generator for evaluation points on X and Y
+  await writeBigInt(fdRS, Fr.toObject(omegaX), n8r);
+  await writeBigInt(fdRS, Fr.toObject(omegaY), n8r);
+
+  // Test code 2 //
+  if (TESTFLAG) {
+    console.log(`Running Test 2`);
+    assert(Fr.eq(omegaX, Fr.e(Fr.toObject(omegaX))));
+    console.log(`Test 2 finished`);
+  }
+  // End of test code 2 //
+
+  await endWriteSection(fdRS);
+  // / End of parameters section
+
+  await fdRS.close();
+
+  const rs = {};
+  rs.curve = curve;
+  rs.n = n;
+  rs.sMax = sMax;
+  rs.omegaX = omegaX;
+  rs.omegaY = omegaY;
+
+  partTime = timer.start();
+
+  console.log(
+      `Generating Lagrange bases for X with ${n} evaluation points...`,
+  );
+  const lagrangeBasis = await polyUtils.buildCommonPolys(rs);
+  console.log(
+      `Generating Lagrange bases for X with ${n} evaluation points...Done`,
+  );
+
+  let FSTimeAccum = 0;
+  for (let k=0; k<sD; k++) {
+    console.log(`Interpolating ${3*m[k]} QAP polynomials...${k+1}/${sD}`);
+    const {
+      uX: uX,
+      vX: vX,
+      wX: wX,
+    } = await polyUtils.buildR1csPolys(
+        curve,
+        lagrangeBasis,
+        r1cs[k],
+        sR1cs[k],
+    );
+
+    console.log(`File writing the polynomials...`);
+    const FSTime = timer.start();
+    const fdQAP = await createBinFile(
+        `${dirPath}/subcircuit${k}.qap`,
+        'qapp',
+        1,
+        2,
+        1<<22,
+        1<<24,
+    );
+
+    await startWriteSection(fdQAP, 1);
+    await fdQAP.writeULE32(1); // Groth
+    await endWriteSection(fdQAP);
+
+    await startWriteSection(fdQAP, 2);
+    for (let i=0; i<m[k]; i++) {
+      const degree = uX[i].length;
+      await fdQAP.writeULE32(degree);
+      for (let xi=0; xi<degree; xi++) {
+        if (typeof uX[i][xi][0] != 'bigint') {
+          await fdQAP.write(uX[i][xi][0]);
+        } else {
+          await writeBigInt(fdQAP, uX[i][xi][0], n8r);
+        }
+      }
+    }
+    for (let i=0; i<m[k]; i++) {
+      const degree = vX[i].length;
+      await fdQAP.writeULE32(degree);
+      for (let xi=0; xi<degree; xi++) {
+        if (typeof vX[i][xi][0] != 'bigint') {
+          await fdQAP.write(vX[i][xi][0]);
+        } else {
+          await writeBigInt(fdQAP, vX[i][xi][0], n8r);
+        }
+      }
+    }
+    for (let i=0; i<m[k]; i++) {
+      const degree = wX[i].length;
+      await fdQAP.writeULE32(degree);
+      for (let xi=0; xi<degree; xi++) {
+        if (typeof wX[i][xi][0] != 'bigint') {
+          await fdQAP.write(wX[i][xi][0]);
+        } else {
+          await writeBigInt(fdQAP, wX[i][xi][0], n8r);
+        }
+      }
+    }
+    await endWriteSection(fdQAP);
+    await fdQAP.close();
+    FSTimeAccum += timer.end(FSTime);
+  }
+  const qapTime = timer.end(partTime);
+  const totalTime = timer.end(startTime);
+
+  console.log(' ');
+  console.log('-----Build QAP Time Analyzer-----');
+  console.log(`###Total ellapsed time: ${totalTime} [ms]`);
+  console.log(` ##R1CS loading time: ${r1csTime} [ms] (${r1csTime/totalTime*100} %)`);
+  console.log(` ##Total QAP time for ${m.reduce((accu, curr) => accu + curr)} wires: ${qapTime} [ms] (${qapTime/totalTime*100} %)`);
+  console.log(`  #QAP interpolation time: ${qapTime-FSTimeAccum} [ms] (${(qapTime-FSTimeAccum)/totalTime*100} %)`);
+  console.log(`  #QAP file writing time: ${FSTimeAccum} [ms] (${FSTimeAccum/totalTime*100} %)`);
+  console.log(` ##Average QAP time per wire with ${n} interpolation points: ${qapTime/m.reduce((accu, curr) => accu + curr)} [ms]`);
+}
