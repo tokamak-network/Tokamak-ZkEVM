972a482fb1da2bc01ac87eaadfd2a68df5442614
Youn <pleiadex.dev@gmail.com>
2023-03-10 00:08:15 +0900

    sub function test

--- test/functions/test_functions.js
+++ test/functions/test_functions.js
@@ -4,6 +4,7 @@ import chai from 'chai';
 const assert = chai.assert;
 
 import testcases_add from './testdata/testcases_add.json' assert {type: 'json'};
+import testcases_sub from './testdata/testcases_sub.json' assert {type: 'json'};
 
 const RunState = {
   opcode: 0x00,
@@ -15,9 +16,20 @@ const RunState = {
 describe("0x01 ADD function", () => {
   testcases_add.forEach(({ X, Y, Expected }) => {
     it("should add two numbers", () => {
-      RunState.stack.push(BigInt('0x' + Y))
       RunState.stack.push(BigInt('0x' + X))
-      functions.get(0x01)(RunState)
+      RunState.stack.push(BigInt('0x' + Y))
+      functions.get(0x01)(RunState) // X + Y
+      assert.equal(RunState.stack.pop(), BigInt('0x' + Expected))
+    })
+  })
+})
+
+describe("0x03 SUB function", () => {
+  testcases_sub.forEach(({ X, Y, Expected }) => {
+    it("should subtract two numbers", () => {
+      RunState.stack.push(BigInt('0x' + X))
+      RunState.stack.push(BigInt('0x' + Y))
+      functions.get(0x03)(RunState) // Y - X
       assert.equal(RunState.stack.pop(), BigInt('0x' + Expected))
     })
   })
