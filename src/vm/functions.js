470b5b6d19b316cee8c70c71484f40e4716189ce
Youn <pleiadex.dev@gmail.com>
2023-03-10 00:34:28 +0900

    resolve imporing file path

--- src/vm/functions.js
+++ src/vm/functions.js
@@ -1,5 +1,5 @@
-import { keccak256 } from 'ethereum-cryptography/keccak'
-import { bytesToHex } from 'ethereum-cryptography/utils'
+import { keccak256 } from 'ethereum-cryptography/keccak.js'
+import { bytesToHex } from 'ethereum-cryptography/utils.js'
 
 import {
   TWO_POW256,
