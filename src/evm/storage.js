bacb3041a38213350a67cfcd48c7879ef592ae6c
Sungyoun Seo <pleiadex.dev@gmail.com>
2023-03-27 23:02:51 -0700

    add storage

---
+++ src/evm/storage.js
@@ -0,0 +1,70 @@
+import { Address } from '@ethereumjs/util'
+
+export class TransientStorage {
+
+  constructor () {
+    /**
+     * The current values of the transient storage, keyed by contract address and then slot
+     */
+    this._storage = new Map()
+    /**
+     * Each change to storage is recorded in the journal. This is never cleared.
+     */
+    this._changeJournal = []
+    /**
+     * The length of the journal at the beginning of each call in the call stack.
+     */
+    this._indices = [0]
+  }
+
+  /**
+   * Get the value for the given address and key
+   * @param {Address} addr the address for which transient storage is accessed
+   * @param {Buffer} key the key of the address to get
+   * @returns {Buffer} the value of the transient storage slot
+   */
+  get(addr, key) {
+    const map = this._storage.get(addr.toString())
+    if (!map) {
+      return Buffer.alloc(32)
+    }
+    const value = map.get(key.toString('hex'))
+    if (!value) {
+      return Buffer.alloc(32)
+    }
+    return value
+  }
+
+  /**
+   * Put the given value for the address and key
+   * @param {Address} addr the address of the contract for which the key is being set
+   * @param {Buffer} key the slot to set for the address
+   * @param {Buffer} value the new value of the transient storage slot to set
+   */
+  put(addr, key, value) {
+    if (key.length !== 32) {
+      throw new Error('Transient storage key must be 32 bytes long')
+    }
+
+    if (value.length > 32) {
+      throw new Error('Transient storage value cannot be longer than 32 bytes')
+    }
+
+    const addrString = addr.toString()
+    if (!this._storage.has(addrString)) {
+      this._storage.set(addrString, new Map())
+    }
+    const map = this._storage.get(addrString)
+
+    const keyStr = key.toString('hex')
+    const prevValue = map.get(keyStr) ?? Buffer.alloc(32)
+
+    this._changeJournal.push({
+      addr: addrString,
+      key: keyStr,
+      prevValue,
+    })
+
+    map.set(keyStr, value)
+  }
+}
