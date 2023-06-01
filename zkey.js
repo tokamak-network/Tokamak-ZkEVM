082d3abc1c3ddccafdbda973c60d839b3717e811
JehyukJang <109242826+JehyukJang@users.noreply.github.com>
2023-06-01 14:59:45 +0900

    Delete zkey.js

--- zkey.js
+++
@@ -1,6 +0,0 @@
-export {default as setup} from './setup.js';
-export {default as derive} from './derive.js';
-export {default as groth16Prove} from './prove.js';
-export {default as groth16Verify} from './verify.js';
-export {default as buildQAP} from './build_qap.js';
-export {default as buildSingleQAP} from './build_single_qap.js';
